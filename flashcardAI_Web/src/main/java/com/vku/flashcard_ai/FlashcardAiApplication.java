56d77c29634ebc199336bb9baa1a226cb290660a
LONGDKZdev <longvh2549@gmail.com>
2026-08-03 20:25:35 +0700

    Fist commit

---
+++ flashcardAI_Web/src/main/java/com/vku/flashcard_ai/FlashcardAiApplication.java
@@ -0,0 +1,13 @@
+package com.vku.flashcard_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardAiApplication.class, args);
+	}
+
+}
---
+++ flashcardAI_Web/src/test/java/com/vku/flashcard_ai/FlashcardAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vku.flashcard_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
